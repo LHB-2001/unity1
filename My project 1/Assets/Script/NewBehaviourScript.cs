1986aef9f62072bbc1b281c97d1d5a4855632b01
liuhongbing <74175832+LHB-2001@users.noreply.github.com>
2026-01-20 15:31:32 +0800

    Update NewBehaviourScript.cs

--- My project 1/Assets/Script/NewBehaviourScript.cs
+++ My project 1/Assets/Script/NewBehaviourScript.cs
@@ -7,16 +7,5 @@ public class NewBehaviourScript : MonoBehaviour
     public int a = 10;
     public int b = 20;
     public int c = a + b;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log(c);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        c = a + b;
-        Debug.Log(c);
-    }
+    
 }
